4f28cd56b9e41acefec68d24adeeaf0e2ac046aa
Jeremy Carbaugh <jcarbaugh@gmail.com>
2011-08-29 15:40:27 -0400

    add __unicode__, Meta, and updated fixture

--- thecleanest/schedule/models.py
+++ thecleanest/schedule/models.py
@@ -6,14 +6,35 @@ class NamelessWorker(models.Model):
     email = models.EmailField()
     is_active = models.BooleanField(default=False)
 
+    class Meta:
+        ordering = ('last_name', 'first_name')
+
+    def __unicode__(self):
+        return self.full_name()
+
+    def full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
 class Assignment(models.Model):
     date = models.DateField(null=False, blank=False, unique=True)
     worker = models.ForeignKey(NamelessWorker, null=False)
+
+    class Meta:
+        ordering = ('date',)
+
+    def __unicode__(self):
+        return "%s %s" % (self.date, self.worker.full_name())
 
 class Debit(models.Model):
     worker = models.ForeignKey(NamelessWorker, related_name='debits', null=False)
     skipped_date = models.DateField(null=False)
     timestamp = models.DateTimeField(null=False)
+
+    class Meta:
+        ordering = ('timestamp',)
+
+    def __unicode__(self):
+        return "%s skipped %s" % (self.worker.full_name(), self.skipped_date)
 
 class Credit(models.Model):
     # debit is null if the credit was created by decree
@@ -24,6 +45,9 @@ class Credit(models.Model):
     note = models.CharField(max_length=5000, null=True)
     timestamp = models.DateTimeField(null=False)
 
+    class Meta:
+        ordering = ('timestamp',)
 
-
+    def __unicode__(self):
+        return "%s credited to %s" % (self.debit, self.worker.full_name())
 
