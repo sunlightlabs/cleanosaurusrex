4707b984156903488d8095a410fe35df7ca48f57
Drew Vogel <dvogel@sunlightfoundation.com>
2011-08-29 16:15:41 -0400

    Added coupon model.

--- thecleanest/schedule/models.py
+++ thecleanest/schedule/models.py
@@ -16,14 +16,15 @@ class Debit(models.Model):
     timestamp = models.DateTimeField(null=False)
 
 class Credit(models.Model):
-    # debit is null if the credit was created by decree
     debit = models.ForeignKey(Debit, null=True)
     worker = models.ForeignKey(NamelessWorker, related_name='credits', null=False)
     # skipped_date is set when the generation function uses the credit
     skipped_date = models.DateField(default=None, null=True)
-    note = models.CharField(max_length=5000, null=True)
     timestamp = models.DateTimeField(null=False)
 
-
-
+class Coupon(models.Model):
+    worker = models.ForeignKey(NamelessWorker, related_name='coupons', null=False)
+    skipped_date = models.DateField(default=None, null=True)
+    note = models.CharField(max_length=5000, null=True)
+    timestamp = models.DateTimeField(null=False)
 
