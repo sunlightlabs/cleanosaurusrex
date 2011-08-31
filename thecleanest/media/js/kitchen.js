3eaccbd20739e1dcac224dbe56993e490b796384
Drew Vogel <dvogel@sunlightfoundation.com>
2011-08-31 10:26:48 -0400

    CSS fixed for popover

--- thecleanest/media/js/kitchen.js
+++ thecleanest/media/js/kitchen.js
@@ -15,21 +15,25 @@
 
     function confirm_nudge () {
         $("#mask").show();
+        $("#overlay").show();
         $("#pop-over").show();
     }
 
     function dismiss_nudge_dialog () {
         $("#mask").hide();
+        $("#overlay").hide();
         $("#pop-over").hide();
     }
 
     function confirm_bone () {
         $("#mask").show();
+        $("#overlay").show();
         $("#pop-over2").show();
     }
 
     function dismiss_bone_dialog () {
         $("#mask").hide();
+        $("#overlay").hide();
         $("#pop-over2").hide();
     }
 
