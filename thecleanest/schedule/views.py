a0b2dc6b56a0eff307236f4dd6417366b1cf12c6
Drew Vogel <dvogel@sunlightfoundation.com>
2011-08-31 10:56:51 -0400

    Added schedule page. Fixed font url in CSS.

--- thecleanest/schedule/views.py
+++ thecleanest/schedule/views.py
@@ -1,10 +1,11 @@
 
-from datetime import date, timedelta
+from datetime import datetime, date, timedelta
 import calendar
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response, render
 from django.core.serializers import serialize, deserialize
 from schedule.models import Assignment, Debit, Credit
+from notifications.models import Bone, Nudge
 
 def defer_assignment(request, defer_code):
 
@@ -30,14 +31,24 @@ def current_schedule(request):
     assignment = Assignment.objects.current_assignment()
     today = date.today()
     today_weekday = calendar.weekday(today.year, today.month, today.day)
+    today_range = (datetime(today.year, today.month, today.day, 0, 0, 0),
+                   datetime(today.year, today.month, today.day, 23, 59, 59))
     monday = date.today() - timedelta(days=today_weekday)
     assignments = Assignment.objects.filter(date__gte=monday).order_by('date')[:10]
+    week1_assignments = assignments[0:5]
+    week2_assignments = assignments[5:10]
+    bone_count = Bone.objects.filter(timestamp__range=today_range).count()
+    nudge_count = Nudge.objects.filter(timestamp__range=today_range).count()
 
     return render_to_response('schedule.html', {
                                   'today': str(today),
                                   'monday': str(monday),
                                   'assignments': assignments,
-                                  'current_assignment': assignment
+                                  'week1_assignments': week1_assignments,
+                                  'week2_assignments': week2_assignments,
+                                  'current_assignment': assignment,
+                                  'bone_count': bone_count,
+                                  'nudge_count': nudge_count
                               })
 
 def kitchen(request):
