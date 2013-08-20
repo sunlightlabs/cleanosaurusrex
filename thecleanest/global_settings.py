0f78d034e9a8a3e72ed689fcd8b01509f642cdcf
Drew Vogel <dvogel@sunlightfoundation.com>
2013-08-20 09:52:56 -0400

    Updated excused list.

--- thecleanest/global_settings.py
+++ thecleanest/global_settings.py
@@ -82,5 +82,6 @@ EXCUSED = [
     'tneale@sunlightfoundation.com',
     'ethan@sunlightfoundation.com',
     'bpease@sunlightfoundation.com',
+    'tknuutila@sunlightfoundation.com',
     'showell@sunlightfoundation.com',
 ]
